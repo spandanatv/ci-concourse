42538caa5db387d7f041ef22eddca241677931df
spandanatv <spandanat@vedams.com>
2018-01-22 15:05:16 +0530

    Update build.sh

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
-set -e
-
-export ROOT_FOLDER=$( pwd )
-export REPO=repo
-
-SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-. ${SCRIPTS_DIR}/generate-settings.sh
-
-[[ -f "${SCRIPTS_DIR}/functions.sh" ]] && source "${SCRIPTS_DIR}/functions.sh" || \
-    echo "No functions.sh found"
-
-
-cd ${ROOT_FOLDER}/${REPO}
-build "${PROJECT_TYPE}"
+cd repo
+gradle clean assemble
